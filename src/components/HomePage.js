25e532af04d78b386965cd5903804f41f9da0014
Klair8 <clsacuto@gmail.com>
2023-03-31 15:47:19 +0300

    Homepage1/2 done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,25 +5,31 @@ import {Link} from 'react-router-dom'
 
 const HomePage =()=>{
     return(
-        <div>
-            <h1>Homepage</h1>
-            <NavBar/>
+            <div className="Homepage">
+                 <NavBar/>
+                <div className="section">
             <div className="BrbyAge">
                 <h1>Browse By Age </h1>
+                <br></br>
                 <Link to="/stories/age-3-5">  
                 <button> Stories for kids Ages 3-5 </button>
                 </Link> 
+                <br></br>
                 <Link to="/stories/age-3-5">  
                 <button> Stories for kids Ages 5-8 </button>
                 </Link> 
+                <br></br>
                 <Link to="/stories/age-3-5">  
                 <button> Stories for kids Ages 8-10 </button>
                 </Link> 
             </div>
             <div className="BrbyAge">
                 <h1>Browse By Style </h1>
+                <br></br>
                 <button> Adventures</button>
+                <br></br>
                 <button> Spaces </button>
+            </div>
             </div>
         </div>
     )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,22 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link} from 'react-router-dom'
 
 function NavBar() {
     return (
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar bg="light" expand="lg" class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <Container>       
+          <Navbar.Brand >Dream Learning</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link to ={`/`}>Home</Nav.Link>
+              <Nav.Link href="#link">kids-stories</Nav.Link>
+              <Nav.Link href="#About">About</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
