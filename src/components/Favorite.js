543be226b4206b5790ccbb014bc4ec3d7b192b6f
Klair8 <clsacuto@gmail.com>
2023-04-16 12:06:43 +0300

    Favorite done

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -16,29 +16,37 @@ const Favorite = () => {
     localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
     };
 
-
     return(
         <div>
             <NavBar/>
         <div className='favorite'>
         <h1> Favorite Stories </h1>
+        <div className='favoritetable'>
+        <table>
+        <tr>
+          <th>TITLE</th>
+          <th>STORY TIME </th>
+          <th>DELETE</th>
+        </tr>
          {
             favorites.map(items =>(
                 favorites && items && items[0] &&
-                <div className="favoritetable" key={items[0].id}>
-                    <table>
-                        <tr>
-                    <th><h5>{items[0].title}</h5></th>
-                    <th><button className='button' onClick={() => handleDelete(items[0].id)}> Delete</button></th> 
-                    <th><button className='button'> <Link to ={`/story/${(items[0].id)}`} state={(items[0].id)} onClick={() => console.log('story.id', (items[0].id))}> Read</Link></button></th> 
-                        </tr>
-                    </table>
-                    </div>
-            ))
-         }
+                <tr key={items[0].id}>
+                    <td><h5>{items[0].title}</h5></td>
+                    <td><button className='buttonFav'> <Link to ={`/story/${(items[0].id)}`} state={(items[0].id)} onClick={() => console.log('story.id', (items[0].id))}> Read </Link></button></td> 
+                    <td><button className='buttonFav' onClick={() => handleDelete(items[0].id)}> Delete </button></td> 
+                    </tr>
+               )
+            )
+            }
+          </table>
+        </div>
         </div>
         </div>
     )
 }
+
+
+
 
 export default Favorite
