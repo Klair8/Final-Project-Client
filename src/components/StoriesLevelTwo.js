a1c1f3f426b871082bc4ec14f3d1eca7d57d1113
Klair8 <clsacuto@gmail.com>
2023-04-17 03:20:16 +0300

    last update

--- src/components/StoriesLevelTwo.js
+++ src/components/StoriesLevelTwo.js
@@ -14,7 +14,7 @@ const StoriesLevelTwo =(props)=>{
             const data = await res.json()
             const specificStories = data.filter(story => story.level == "level2" )
         setStories(specificStories);
-         console.log('datalevel2',specificStories)
+         console.log('datalevel3',specificStories)
         } catch(e){
             console.log(e)
         }
@@ -25,30 +25,30 @@ const StoriesLevelTwo =(props)=>{
     return(
         <div>
           <NavBar/>
-            <div className="storiespage">
-              <h1>Stories for ages 5-6 </h1>
-            <div className="storycontainer">
-             <br></br>
+          <div className="storiespage">
+            <h1>Stories for ages 5-6 </h1>
+          <div className="storycontainer">
+            <br></br>
             {
               stories.map(story=>{
                 return(
                   <Card className="card"  key={story.id} style={{ width: '18rem', height:'26rem' , border:'2px solid black' }}>
-                    <Card.Img variant="top" src={`${process.env.REACT_APP_SERVER_URL}${story.image_url}}`} /> 
+                    <Card.Img variant="top" src={`${process.env.REACT_APP_SERVER_URL}${story.image_url}`} /> 
                     <Card.Body>
                       <Card.Title>{story.title}</Card.Title>
                       <Card.Text>{story.description}</Card.Text>
-                      <br></br>
                       <button className='buttonRead'><Link to ={`/story/${story.id}`} state={story.id} onClick={() => console.log('story.id', story.id)}>Read</Link></button>
-                      </Card.Body>  
+                    </Card.Body>  
                   </Card>
                   )  
                 })
             }
-            </div>
-            </div>
+          </div>
+          </div>
         </div>
     )
 }
+
 
 
 export default StoriesLevelTwo
